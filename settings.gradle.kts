53f6afa4ce1c8b7f8894cd79f10313fd8e8750be
Leanid Piliptsevich <pilleo19@gmail.com>
2026-07-23 10:37:49 +0200

    feat(ci): implement master-only Gradle build-cache saving & path-based filtering for slow steps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,12 @@
 // Its primary purpose is to define the subprojects.
 // It is also used for some aspects of project-wide configuration, like managing plugins, dependencies, etc.
 // https://docs.gradle.org/current/userguide/settings_file_basics.html
+buildCache {
+    local {
+        isEnabled = true
+    }
+}
+
 dependencyResolutionManagement {
     // Use Maven Central as the default repository (where Gradle will download dependencies) in all subprojects.
     repositories {
